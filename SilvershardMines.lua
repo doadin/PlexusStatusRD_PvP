a8f3483024d0573c3d4c26ba65208035d3a12844
tbkizle <tbkizle@gmail.com>
2021-01-09 06:21:02 -0500

    First Build

---
+++ SilvershardMines.lua
@@ -0,0 +1,45 @@
+-- local zone = "Silvershard Mines"
+local zoneid = 6126
+
+--Death Knight
+GridStatusRaidDebuff:DebuffId(zoneid, 45524, 1, 5, 5) --Chains of Ice
+--Demon Hunter
+GridStatusRaidDebuff:DebuffId(zoneid, 217832, 1, 5, 5) --Imprison
+GridStatusRaidDebuff:DebuffId(zoneid, 344867, 1, 5, 5) --Chaos Nova
+--Druid
+GridStatusRaidDebuff:DebuffId(zoneid, 33786, 1, 5, 5) --Cyclone
+GridStatusRaidDebuff:DebuffId(zoneid, 2637, 1, 5, 5) --Hibernate
+GridStatusRaidDebuff:DebuffId(zoneid, 339, 1, 5, 5) --Entangling Roots
+--Hunter
+GridStatusRaidDebuff:DebuffId(zoneid, 187650, 1, 5, 5) --Freezing Trap
+GridStatusRaidDebuff:DebuffId(zoneid, 187650, 1, 4, 4) --Tar Trap
+GridStatusRaidDebuff:DebuffId(zoneid, 1513, 1, 4, 4) --Scare Beast
+--Mage
+GridStatusRaidDebuff:DebuffId(zoneid, 118, 1, 5, 5) --Polymorph
+GridStatusRaidDebuff:DebuffId(zoneid, 122, 1, 5, 5) --Frost Nova
+--Monk
+GridStatusRaidDebuff:DebuffId(zoneid, 115078, 1, 5, 5) --Paralysis
+GridStatusRaidDebuff:DebuffId(zoneid, 119381, 1, 5, 5) --Leg Sweep
+--Paladin
+GridStatusRaidDebuff:DebuffId(zoneid, 10326, 1, 5, 5) --Turn Evil
+GridStatusRaidDebuff:DebuffId(zoneid, 853, 1, 5, 5) --Hammer of Justice
+--Priest
+GridStatusRaidDebuff:DebuffId(zoneid, 605, 1, 5, 5) --Mind Control
+GridStatusRaidDebuff:DebuffId(zoneid, 8122, 1, 5, 5) --Psychic Scream
+--Rogue
+GridStatusRaidDebuff:DebuffId(zoneid, 2094, 1, 5, 5) --Blind
+GridStatusRaidDebuff:DebuffId(zoneid, 408, 1, 5, 5) --Kidney Shot
+GridStatusRaidDebuff:DebuffId(zoneid, 6770, 1, 5, 5) --Sap
+GridStatusRaidDebuff:DebuffId(zoneid, 1833, 1, 5, 5) --Cheap Shot
+--Shaman
+GridStatusRaidDebuff:DebuffId(zoneid, 51514, 1, 5, 5) --Hex
+GridStatusRaidDebuff:DebuffId(zoneid, 192058, 1, 5, 5) --Capacitor Totem
+GridStatusRaidDebuff:DebuffId(zoneid, 196840, 1, 5, 5) --Frost Shock
+GridStatusRaidDebuff:DebuffId(zoneid, 2484, 1, 5, 5) --Earthbind Totem
+--Warlock
+GridStatusRaidDebuff:DebuffId(zoneid, 5782, 1, 5, 5) --Fear
+GridStatusRaidDebuff:DebuffId(zoneid, 30283, 1, 5, 5) --Shadowfury
+GridStatusRaidDebuff:DebuffId(zoneid, 710, 1, 5, 5) --Banish
+GridStatusRaidDebuff:DebuffId(zoneid, 6358, 1, 5, 5) --Seduction
+--Warrior
+GridStatusRaidDebuff:DebuffId(zoneid, 5246, 1, 5, 5) --Intimidating Shout
