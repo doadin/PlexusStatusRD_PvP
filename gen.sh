d4fe65a1dafce06fe1aebb8064bfcdb574d8e5cb
tbkizle <tbkizle@gmail.com>
2021-01-10 14:11:38 -0500

    Test Build

---
+++ gen.sh
@@ -0,0 +1,46 @@
+for f in *.lua
+do
+    printf "\n\n" >> $f
+    printf "\n--Death Knight\n" >> $f
+    printf "\n--GridStatusRaidDebuff:DebuffId(zoneid, 45524, 1, 5, 5) --Chains of Ice\n" >> $f
+    printf "\n--Demon Hunter\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 217832, 1, 5, 5) --Imprison\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 344867, 1, 5, 5) --Chaos Nova\n" >> $f
+    printf "\n--Druid\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 33786, 1, 5, 5) --Cyclone\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 2637, 1, 5, 5) --Hibernate\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 339, 1, 5, 5) --Entangling Roots\n" >> $f
+    printf "\n--Hunter\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 187650, 1, 5, 5) --Freezing Trap\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 187650, 1, 4, 4) --Tar Trap\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 1513, 1, 4, 4) --Scare Beast\n" >> $f
+    printf "\n--Mage\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 118, 1, 5, 5) --Polymorph\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 122, 1, 5, 5) --Frost Nova\n" >> $f
+    printf "\n--Monk\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 115078, 1, 5, 5) --Paralysis\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 119381, 1, 5, 5) --Leg Sweep\n" >> $f
+    printf "\n--Paladin\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 10326, 1, 5, 5) --Turn Evil\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 853, 1, 5, 5) --Hammer of Justice\n" >> $f
+    printf "\n--Priest\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 605, 1, 5, 5) --Mind Control\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 8122, 1, 5, 5) --Psychic Scream\n" >> $f
+    printf "\n--Rogue\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 2094, 1, 5, 5) --Blind\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 408, 1, 5, 5) --Kidney Shot\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 6770, 1, 5, 5) --Sap\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 1833, 1, 5, 5) --Cheap Shot\n" >> $f
+    printf "\n--Shaman\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 51514, 1, 5, 5) --Hex\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 192058, 1, 5, 5) --Capacitor Totem\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 196840, 1, 5, 5) --Frost Shock\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 2484, 1, 5, 5) --Earthbind Totem\n" >> $f
+    printf "\n--Warlock\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 5782, 1, 5, 5) --Fear\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 30283, 1, 5, 5) --Shadowfury\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 710, 1, 5, 5) --Banish\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 6358, 1, 5, 5) --Seduction\n" >> $f
+    printf "\n--Warrior\n" >> $f
+    printf "\nGridStatusRaidDebuff:DebuffId(zoneid, 5246, 1, 5, 5) --Intimidating Shout\n" >> $f
+done
